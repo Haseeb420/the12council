cf19c9d49d33b0faaca20074905217918eb8fd06
Muhammad Haseeb <muhammadhaseebtcf@gmail.com>
2024-09-02 11:58:26 +0500

    updated css

--- src/components/CenteredImage.jsx
+++ src/components/CenteredImage.jsx
@@ -3,7 +3,7 @@ import headerImage from "../assets/logo.png";
 function CenteredImage() {
   return (
     <div className="">
-      <img src={headerImage} alt="Header" className="header-image" loading="lazy"/>
+      <img src={headerImage} alt="Header" className="header-image"/>
       <h1 className="text-4xl font-bold p-0 text-center mb-4">The 12 Councils</h1>
     </div>
   );
