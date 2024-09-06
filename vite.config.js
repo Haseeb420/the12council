cd5282b79d67b3b18356513b52d5c3886efec78b
Muhammad Haseeb <muhammadhaseebtcf@gmail.com>
2024-09-06 23:32:21 +0500

    dd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: "/the12council",
+  base: "/",
   plugins: [react()],
   css: {
     postcss: {
